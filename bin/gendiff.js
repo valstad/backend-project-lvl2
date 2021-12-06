b8e45f0e261d3f7ac16a537e8fb47745e9a8e1ae
Konstantin Firsov <firsov.l@gmail.com>
2021-12-06 14:05:45 +0400

    add filepath args and -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,12 @@
 import commander from 'commander';
 const program = new commander.Command();
 
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
+
 
 program.parse(process.argv);
