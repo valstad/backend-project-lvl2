741e442baacf7c5a69d258fcc5d368caad99a612
Konstantin Firsov <firsov.k@gmail.com>
2021-12-07 12:28:43 +0400

    fix import path error at intex.text.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,11 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
 import { readFileSync } from 'fs';
+import path from 'path';
 import diff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('init', () => {
