798c58cab04b419e5f1c813ba7a9f157b19ef004
Konstantin Firsov <firsov.k@gmail.com>
2021-12-10 15:51:09 +0400

    replace if with case in formatters

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,33 +1,27 @@
 import _ from 'lodash';
 import { isObject } from '../util.js';
 
-const stringify = (data) => {
-  const f = (val) => {
-    if (isObject(val)) return '[complex value]';
-    if (_.isString(val)) return `'${val}'`;
-    return val;
-  };
+const f = (val) => {
+  if (isObject(val)) return '[complex value]';
+  if (_.isString(val)) return `'${val}'`;
+  return val;
+};
 
+export default (data) => {
   const iter = (tree, path, init) => Object
     .entries(tree)
     .reduce((acc, [key, value]) => {
-      const nextType = value.type;
       const newPath = [...path, key];
-      if (nextType === 'nested') {
-        return iter(value.value, newPath, acc);
+      switch (value.type) {
+        case 'nested': return iter(value.value, newPath, acc);
+        case 'added':
+          return [...acc, `Property '${newPath.join('.')}' was added with value: ${f(value.valueAdd)}`];
+        case 'removed':
+          return [...acc, `Property '${newPath.join('.')}' was removed`];
+        case 'updated':
+          return [...acc, `Property '${newPath.join('.')}' was updated. From ${f(value.valueRem)} to ${f(value.valueAdd)}`];
+        default: return acc;
       }
-      if (nextType === 'added') {
-        return [...acc, `Property '${newPath.join('.')}' was added with value: ${f(value.valueAdd)}`];
-      }
-      if (nextType === 'removed') {
-        return [...acc, `Property '${newPath.join('.')}' was removed`];
-      }
-      if (nextType === 'updated') {
-        return [...acc, `Property '${newPath.join('.')}' was updated. From ${f(value.valueRem)} to ${f(value.valueAdd)}`];
-      }
-      return acc;
     }, init);
   return iter(data, [], []).join('\n');
 };
-
-export default stringify;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,12 +1,13 @@
 import { isObject } from '../util.js';
 
-const stringify = (data, replacer = ' ', spacesCount = 4) => {
+const f = (tab1, tab2, key, val) => (
+  `${tab1}${tab2}${key}: ${val}`
+);
+
+export default (data, replacer = ' ', spacesCount = 4) => {
   const spacer = replacer.repeat(spacesCount);
   const spaceAdd = `${replacer.repeat(spacesCount - 2)}+ `;
   const spaceRem = `${replacer.repeat(spacesCount - 2)}- `;
-  const f = (tab1, tab2, key, val) => (
-    `${tab1}${tab2}${key}: ${val}`
-  );
 
   const iter = (tree, depth) => {
     if (!isObject(tree)) {
@@ -16,21 +17,19 @@ const stringify = (data, replacer = ' ', spacesCount = 4) => {
     const result = Object
       .entries(tree)
       .map(([key, value]) => {
-        const nextType = value.type;
-        if (nextType === 'nested') {
-          return f(tab, spacer, key, iter(value.value, depth + 1));
+        switch (value.type) {
+          case 'nested': return f(tab, spacer, key, iter(value.value, depth + 1));
+          case 'added': return f(tab, spaceAdd, key, iter(value.valueAdd, depth + 1));
+          case 'removed': return f(tab, spaceRem, key, iter(value.valueRem, depth + 1));
+          case 'updated': return [
+            f(tab, spaceRem, key, iter(value.valueRem, depth + 1)),
+            f(tab, spaceAdd, key, iter(value.valueAdd, depth + 1)),
+          ].join('\n');
+          case 'intacted': return f(tab, spacer, key, value.value);
+          default: return f(tab, spacer, key, iter(value, depth + 1));
         }
-        const rem = f(tab, spaceRem, key, iter(value.valueRem, depth + 1));
-        const add = f(tab, spaceAdd, key, iter(value.valueAdd, depth + 1));
-        if (nextType === 'added') return add;
-        if (nextType === 'removed') return rem;
-        if (nextType === 'updated') return [rem, add].join('\n');
-        if (nextType === 'intacted') return f(tab, spacer, key, value.value);
-        return f(tab, spacer, key, iter(value, depth + 1));
       });
     return ['{', ...result, `${tab}}`].join('\n');
   };
   return iter(data, 0);
 };
-
-export default stringify;
